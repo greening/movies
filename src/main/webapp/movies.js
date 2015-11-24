550a54cc3e5ab4ce63467821b9d8d22337c06eff
Daniel R. Greening <dan@greening.org>
2015-11-24 04:25:22 -0800

    Refactor movies.js and separate styles into CSS.

--- src/main/webapp/movies.js
+++ src/main/webapp/movies.js
@@ -5,21 +5,15 @@ $(document).ready(function() {
     $.ajax({
         url: "http://localhost:8080/movies/webapi/movies"
     }).then(function(data) {
+    	var select_tag = 
+    		'  <select class="rating" name="rating">' +
+    		jQuery.map(["1","2","3","4","5","6","7","8","9","10"], function(v,i) {return('    <option value="' + v + '">' + v + '</option>\n'); }).join("") +
+			'  </select>\n';
+    		
     	var newHTML = jQuery.map(data.movies, function(value,index) {
     	    return('<h3>' + $(value).attr("movie_name") + '</h3>\n' +
-    	    		'  <div><img src="' + $(value).attr("image_url") + '" height="180px" width="auto" style="float: left; padding-right: 20px; vertical-align: text-top;"" />\n' +
-    	    		'  <select class="rating" name="rating" style="display: block;">' +
-											'    <option value="1">1</option>\n' +
-											'    <option value="2">2</option>\n' +
-											'    <option value="3">3</option>\n' +
-											'    <option value="4">4</option>\n' +
-											'    <option value="5">5</option>\n' +
-											'    <option value="6">6</option>\n' +
-											'    <option value="7">7</option>\n' +
-											'    <option value="8">8</option>\n' +
-											'    <option value="9">9</option>\n' +
-											'    <option value="10">10</option>\n' +
-										'  </select>\n' +
+    	    		'  <div><img src="' + $(value).attr("image_url") + '" />\n' +
+    	    		select_tag +
     	    		$(value).attr("description") +  '</div>\n');
     	});
     	$(".movies").html(newHTML.join(""));
