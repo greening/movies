926ef4c0c2a63e4053d5777e0b89de0faa524eb6
Daniel R. Greening <dan@greening.org>
2015-11-24 16:00:24 -0800

    Create a realistic test database, with users, movies, reviews.

--- src/test/java/sr/movies/test/HSQLDBLoadTest.java
+++ src/test/java/sr/movies/test/HSQLDBLoadTest.java
@@ -8,22 +8,41 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Connection;
+import java.net.URL;
+
+import org.hsqldb.cmdline.SqlFile;
+import java.io.File;
+import java.io.IOException;
 
 import org.junit.runner.RunWith;
 
 import org.hsqldb.jdbc.JDBCConnection;
 public class HSQLDBLoadTest {
+	public static Connection c;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		Connection c = DriverManager.getConnection("jdbc:hsqldb:mem:movies", "SA", "");
+		c = DriverManager.getConnection("jdbc:hsqldb:mem:movies", "SA", "");
+		String resourceName = "movies.sql";
+        URL r = HSQLDBLoadTest.class.getClassLoader().getResource(resourceName);
+        File file = (r != null? new File(r.getFile()): null);
+        if (!file.isFile())
+            throw new IOException("SQL resource not present: " + resourceName);
+        SqlFile sqlFile = new SqlFile(file);
+        sqlFile.setConnection(c);
+//        sqlFile.addUserVars(sqlVarMap);
+        sqlFile.execute();
+
 //        database = new HsqlDatabase();
 //        database.setUp("freshtomatoes");
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
+		c.close();
 	}
 
 	@Before
@@ -35,8 +54,9 @@ public class HSQLDBLoadTest {
 	}
 
 	@Test
-	public void test() {
-//		fail("Not yet implemented");
+	public void selectTest() throws SQLException {
+      ResultSet result = c.createStatement().executeQuery("select rating from movies WHERE movie_name='Furious 7'");
+      System.out.println("select rating from movies WHERE movie_name='Furious 7', ResultSet is " + (result == null? "NULL" : result.toString()));
 	}
 
 }
