f0b8ffc81e8d2f59a68e4fa65a4e0be7c2e8f383
Daniel R. Greening <dan@greening.org>
2015-12-05 11:04:58 +0100

    Use reduce instead of jQuery.map, if IE 8 support unneeded.

--- src/main/webapp/movies.js
+++ src/main/webapp/movies.js
@@ -5,10 +5,14 @@ $(document).ready(function() {
     $.ajax({
         url: "http://localhost:8080/movies/webapi/movies"
     }).then(function(data) {
-    	var select_tag = 
+// If we are supporting IE 8 or earlier, use this instead:   	var select_tag = 
+//    		'  <select class="rating" name="rating">' +
+//    		jQuery.map(["1","2","3","4","5","6","7","8","9","10"], function(v,i) {return('    <option value="' + v + '">' + v + '</option>\n'); }).join("") +
+//			'  </select>\n';
+    	var select_tag =
     		'  <select class="rating" name="rating">' +
-    		jQuery.map(["1","2","3","4","5","6","7","8","9","10"], function(v,i) {return('    <option value="' + v + '">' + v + '</option>\n'); }).join("") +
-			'  </select>\n';
+    		[1,2,3,4,5,6,7,8,9,10].reduce(function(old,v) {return(old + '    <option value="' + v + '">' + v + '</option>\n')}) +
+    		'  </select>\n';		
     		
     	var newHTML = jQuery.map(data.movies, function(value,index) {
     	    return('<h3>' + $(value).attr("movie_name") + '</h3>\n' +
